bfcefa1aa3340115657c17fea710539a188a4fae
MihaiN <ionut.nicula93@gmail.com>
2017-01-25 16:29:20 +0200

    Transparent instead of Black bar when there is no progress

--- app/src/main/java/com/circularrangebar/CircularRangeBar/CircularRangeBar.java
+++ app/src/main/java/com/circularrangebar/CircularRangeBar/CircularRangeBar.java
@@ -48,7 +48,7 @@ public class CircularRangeBar extends View {
     protected static final float DEFAULT_END_ANGLE = 270f; // Geometric (clockwise, relative to 3 o'clock)
     protected static final int DEFAULT_MAX = 100;
     protected static final int DEFAULT_PROGRESS = 0;
-    protected static final int DEFAULT_CIRCLE_COLOR = Color.DKGRAY;
+    protected static final int DEFAULT_CIRCLE_COLOR = Color.TRANSPARENT;
     protected static final int DEFAULT_CIRCLE_PROGRESS_COLOR = Color.argb(235, 74, 138, 255);
     protected static final int DEFAULT_POINTER_COLOR = Color.argb(235, 74, 138, 255);
     protected static final int DEFAULT_POINTER_HALO_COLOR = Color.argb(135, 74, 138, 255);
